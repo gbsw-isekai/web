0687c8aef1534562b05051f1c796d3f83f43056d
jeongwoo <105404297+lliilili@users.noreply.github.com>
2023-12-22 14:18:54 +0900

    2023/12.22(login&join)Design

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import { login } from "../../lib/login";
 import Cookies from "js-cookie";
+import { Input } from "../../components/ui/input"
+import { Button } from "../../components/ui/button"
+import { Link } from "react-router-dom";
 
 export default function Login() {
 
@@ -24,32 +27,41 @@ export default function Login() {
   }
 
   return (
-    <div>
-      <form onSubmit={sendInfo}>
-        <div>
-          <input type="text" placeholder="로그인" style={{
-            border: "1px solid black"
-          }}
-            name="id"
-            value={info.id}
-            onChange={onChange} />
-        </div>
-        <div>
-          <input type="password" placeholder="비밀번호" style={{
-            border: "1px solid black"
-          }}
-            name="pw"
-            value={info.pw}
-            onChange={onChange} />
-        </div>
-        <div>
-          <button style={{
-            border: "1px solid black"
-          }}>
-            로그인하기
-          </button>
-        </div>
-      </form>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="w-full max-w-xs">
+        <form onSubmit={sendInfo}>
+          <div className="text-center text-2xl font-bold mb-7">
+            Login in to GbswJob
+          </div>
+          <div>
+            <Input type="text" placeholder="로그인"
+              className="mb-1.5"
+              name="id"
+              value={info.id}
+              onChange={onChange} />
+          </div>
+          <div>
+            <Input type="password" placeholder="비밀번호"
+              className="mb-4"
+              name="pw"
+              value={info.pw}
+              onChange={onChange} />
+          </div>
+          <div>
+            <Button className="w-full max-w-xs h-12 text-base font-bold">
+              로그인
+            </Button>
+          </div>
+          <div className="text-center flex justify-center items-center mt-7">
+            <div className="mr-1">
+              계정이 존재하지 않습니까?
+            </div>
+            <Link to="/users/join" className="text-blue-700 font-medium text-base hover:underline">
+              회원가입
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   )
 } 
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { signUp } from "../../lib/signUp";
 import Cookies from "js-cookie";
+import { Input } from "../../components/ui/input"
+import { Button } from "../../components/ui/button"
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const [userInfo, setUserInfo] = useState({
@@ -25,24 +28,37 @@ export default function SignUp() {
   }
 
   return(
-    <div className="container">
-      <form onSubmit={sendInfo}>
-        <div className="">
-          <input type="text" placeholder="userId" className="border" name="id" value={userInfo.id} onChange={onChange}/>
-        </div>
-        <div className="">
-          <input type="text" placeholder="userName" className="border" name="name" value={userInfo.name} onChange={onChange}/>
-        </div>
-        <div className="">
-          <input type="password" placeholder="password" className="border" name="pw" value={userInfo.pw} onChange={onChange}/>
-        </div>
-        <div className="">
-          <input type="text" placeholder="phoneNum" className="border" name="number" value={userInfo.number} onChange={onChange}/>
-        </div>
-        <div className="">
-          <button className="border">submit</button>
-        </div>
-      </form>
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="w-full max-w-xs">
+        <form onSubmit={sendInfo}>
+          <div className="text-center text-2xl font-bold mb-7">
+            Join in to GbswJob
+          </div>
+          <div className="mb-1.5">
+            <Input type="text" placeholder="아이디" className="border" name="id" value={userInfo.id} onChange={onChange}/>
+          </div>
+          <div className="mb-1.5">
+            <Input type="text" placeholder="이름" className="border" name="name" value={userInfo.name} onChange={onChange}/>
+          </div>
+          <div className="mb-1.5">
+            <Input type="password" placeholder="비밀번호" className="border" name="pw" value={userInfo.pw} onChange={onChange}/>
+          </div>
+          <div className="mb-4">
+            <Input type="text" placeholder="전화번호" className="border" name="number" value={userInfo.number} onChange={onChange}/>
+          </div>
+          <div >
+            <Button className="w-full max-w-xs h-12 text-base font-bold">회원가입</Button>
+          </div>
+          <div className="text-center flex justify-center items-center mt-7">
+            <div className="mr-1">
+              계정이 존재합니까?
+            </div>
+            <Link to="/auth/login" className="text-blue-700 font-medium text-base hover:underline">
+              로그인
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   ) 
 }

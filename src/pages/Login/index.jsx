ac921d2b35a4e6501f70c9701eaf247fce6087b8
wjdgotjd <wjdgotjd529@naver.com>
2023-12-27 21:33:18 +0900

    feat: 로그인 후 이전 페이지로 이동

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,29 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import { login } from "../../lib/login";
 import Cookies from "js-cookie";
-import { Input } from "../../components/ui/input"
-import { Button } from "../../components/ui/button"
-import { Link } from "react-router-dom";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-
+  const nagivate = useNavigate();
 
   const [info, setInfo] = useState({
-    id: '',
-    pw: '',
-  })
+    id: "",
+    pw: "",
+  });
 
   function onChange(e) {
     setInfo((prev) => ({
       ...prev,
       [e.target.name]: e.target.value,
-    }))
+    }));
   }
 
   async function sendInfo(e) {
     e.preventDefault();
     const { data } = await login(info);
-    Cookies.set('access_token', data);
+    Cookies.set("access_token", data);
+    nagivate(-1);
   }
 
   return (
@@ -34,18 +35,24 @@ export default function Login() {
             Login in to GbswJob
           </div>
           <div>
-            <Input type="text" placeholder="로그인"
+            <Input
+              type="text"
+              placeholder="로그인"
               className="mb-1.5"
               name="id"
               value={info.id}
-              onChange={onChange} />
+              onChange={onChange}
+            />
           </div>
           <div>
-            <Input type="password" placeholder="비밀번호"
+            <Input
+              type="password"
+              placeholder="비밀번호"
               className="mb-4"
               name="pw"
               value={info.pw}
-              onChange={onChange} />
+              onChange={onChange}
+            />
           </div>
           <div>
             <Button className="w-full max-w-xs h-12 text-base font-bold">
@@ -53,15 +60,16 @@ export default function Login() {
             </Button>
           </div>
           <div className="text-center flex justify-center items-center mt-7">
-            <div className="mr-1">
-              계정이 존재하지 않습니까?
-            </div>
-            <Link to="/users/join" className="text-blue-700 font-medium text-base hover:underline">
+            <div className="mr-1">계정이 존재하지 않습니까?</div>
+            <Link
+              to="/users/join"
+              className="text-blue-700 font-medium text-base hover:underline"
+            >
               회원가입
             </Link>
           </div>
         </form>
       </div>
     </div>
-  )
-} 
+  );
+}

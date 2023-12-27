5c8511f3cc7b16ce351facbd2f8fa5f69a64265d
wjdgotjd <wjdgotjd529@naver.com>
2023-12-27 21:35:46 +0900

    feat: QA페이지 UI 변경

--- src/pages/QA/answer.jsx
+++ src/pages/QA/answer.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom/dist";
 
-function Answer({ nickname, content, isOwner, editUrl }) {
+function Answer({ nickname, content, isOwner, editUrl, className }) {
   return (
-    <div className="border-y-2 border-gray-500">
-      <div className="answer-profile_info">
+    <div className={`bg-white ${className} px-3 py-2`}>
+      <div>
         <div className="flex justify-between">
-          <div className="answer-info_right">
-            <div className="mb-2">{nickname}</div>
-            <div className="flex gap-2 mb-4">
+          <div>
+            <div className="flex items-center">
+              <div className="w-6 h-6 bg-slate-500 rounded-full"></div>
+              <div className="ml-2">{nickname}</div>
+            </div>
+            <div className="flex gap-2 mt-3">
               <div className="answer-definition">채택답변수 4,341</div>
               <div className="answer-thanks">· 받은감사수 35</div>
               <div className="answer-layer">태양신</div>
             </div>
+            {isOwner ? <Link to={editUrl}>수정하기</Link> : null}
           </div>
-          <div className="answer-info_left">answer-profile</div>
         </div>
-        <div className="answer-content whitespace-pre-line">{content}</div>
-        {isOwner ? <Link to={editUrl}>수정하기</Link> : null}
       </div>
+      <div className="mt-4" dangerouslySetInnerHTML={{ __html: content }}></div>
     </div>
   );
 }
--- src/pages/QA/index.jsx
+++ src/pages/QA/index.jsx
@@ -6,7 +6,23 @@ import { Link, useNavigate, useParams } from "react-router-dom/dist";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import useToken from "../../hooks/useToken";
+import Header from "src/components/common/Header";
 
+import {
+  MessageCircle,
+  MessageCircleMore,
+  MessageCircleQuestion,
+  MoreVertical,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../components/ui/dropdown-menu";
+
+import "dayjs/locale/ko";
 dayjs.extend(relativeTime);
 dayjs.locale("ko");
 
@@ -57,60 +73,87 @@ function QA() {
 
   return (
     <div className="flex flex-col">
-      <header className="main-header">
-        <h2 className="text-3xl font-medium">header</h2>
-      </header>
-      <section className="max-w-[1024px] self-center">
-        <div className="flex flex-col border-t border-b">
-          <div className="text-xl mb-2 flex justify-between">
-            <div>Q {data.title}</div>
-            {data.isOwner ? (
-              <div>
-                <Link to={`/question/${questionId}/editor`}>수정하기</Link>
-                <div onClick={onDeleteHandler}>삭제하기</div>
+      <Header />
+      <div className="w-full min-h-[calc(100vh-56.67px)] bg-gray-200">
+        <div className="flex flex-col w-full min-h-full bg-white py-8">
+          <div className="flex flex-col w-full max-w-[1024px] self-center">
+            <div className="mb-2 flex items-center justify-between">
+              <div className="flex items-center">
+                <MessageCircleQuestion className="w-10 h-10" />
+                <div className="ml-2 text-2xl">{data.title}</div>
               </div>
-            ) : null}
-          </div>
-          <div className="mb-5 whitespace-pre-line">{data.content}</div>
-          <div className="mb-6">
-            <a href="/" className="text-blue-800">
-              고등학교진학
-            </a>
-          </div>
-          <div className="flex justify-between">
-            <div className="flex gap-2">
-              <div className="question-profile">profile-icon</div>
-              <div className="question-name">{data.writer.id}</div>
-              <div className="question-date">
-                · {dayjs(data.createdAt).format("YYYY-MM-DD HH:mm:ss")}
-              </div>
-              <div className="question-views">· 조회수 {data.viewCount}</div>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  {data.isOwner ? (
+                    <>
+                      <DropdownMenuItem>
+                        <Link to={`/question/${questionId}/editor`}>
+                          수정하기
+                        </Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <div onClick={onDeleteHandler}>삭제하기</div>
+                      </DropdownMenuItem>
+                    </>
+                  ) : (
+                    <DropdownMenuItem>
+                      <Link to={``}>신고하기</Link>
+                    </DropdownMenuItem>
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-            <div className="flex gap-2">
-              <div className="reply_icon">reply_icon</div>
-              <div className="question_report">report_icon</div>
+            <div
+              className="mb-5 ml-[51px]"
+              dangerouslySetInnerHTML={{ __html: data.content }}
+            />
+            <div className="my-6">
+              <a href="/" className="text-blue-800">
+                고등학교진학
+              </a>
+            </div>
+            <div className="flex justify-between">
+              <div className="flex gap-2">
+                <div className="w-7 h-7 rounded-full bg-gray-400"></div>
+                <div className="question-name">{data.writer.id}</div>
+                <div className="question-date">
+                  · {dayjs(data.createdAt).fromNow()}
+                </div>
+                <div className="question-views">· 조회수 {data.viewCount}</div>
+              </div>
+              <div className="flex gap-2">
+                <div className="">
+                  <MessageCircle />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="answer-title">
-          <div className="mb-3">A {data.answers.length}개</div>
+        <div className="w-full max-w-[1024px] mx-auto my-4 text-xl flex justify-between">
+          <div>A {data.answers.length}개</div>
+          <Link
+            to={`/question/${questionId}/answers/editor`}
+            className="px-3 py-1 bg-green-500 text-white text-base"
+          >
+            답변하기
+          </Link>
         </div>
-        <Link to={`/question/${questionId}/answers/editor`}>
-          <div className="w-full text-center border-y-2 border-gray-800 bg-green-600 text-white">
-            답변작성
-          </div>
-        </Link>
-
-        {data.answers.map((e) => (
-          <Answer
-            key={e.id}
-            nickname={e.writer.id}
-            content={e.content}
-            isOwner={e.writer.id === userId}
-            editUrl={`/question/${questionId}/answers/${e.id}/editor`}
-          />
-        ))}
-      </section>
+        <div>
+          {data.answers.map((e) => (
+            <Answer
+              key={e.id}
+              nickname={e.writer.id}
+              content={e.content}
+              isOwner={e.writer.id === userId}
+              editUrl={`/question/${questionId}/answers/${e.id}/editor`}
+              className="max-w-[1024px] mx-auto"
+            />
+          ))}
+        </div>
+      </div>
       <footer className="main-footer"></footer>
     </div>
   );

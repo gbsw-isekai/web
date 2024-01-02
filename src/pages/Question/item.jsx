62b73161f7711e8fffa66bd0d64e11f799c9c0a4
wjdgotjd <wjdgotjd529@naver.com>
2024-01-02 14:57:44 +0900

    라우팅 변경

--- src/pages/Question/item.jsx
+++ src/pages/Question/item.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 
-
 export default function QuestionItem({
   id,
   title,
@@ -13,33 +12,31 @@ export default function QuestionItem({
   return (
     <div className="p-4 max-w-5xl mx-auto mt-6 border border-solid rounded-xl flex items-center">
       <div className="flex-1">
-          <div className="text-base md:text-lg lg:text-xl mb-3">
-            <Link to={`/question/${id}`} className="text-black hover:underline">
-              {title}
-            </Link>
+        <div className="text-base md:text-lg lg:text-xl mb-3">
+          <Link to={`/questions/${id}`} className="text-black hover:underline">
+            {title}
+          </Link>
+        </div>
+        <div className="flex items-center gap-1">
+          <div>
+            <img
+              src={profile}
+              alt="user-profile"
+              className="w-5 h-5 rounded-full"
+            />
           </div>
-          <div className="flex items-center gap-1">
-            <div>
-              <img src={profile} alt="user-profile" className="w-5 h-5 rounded-full"/>
-            </div>
-            <div className="flex gap-3">
+          <div className="flex gap-3">
+            <div className="text-sm text-gray-600">{writer}</div>
+            <div className="text-sm text-gray-600">조회수 {viewCount}</div>
+            <div className="flex items-center gap-1">
               <div className="text-sm text-gray-600">
-                {writer}
+                <IoChatbubbleEllipsesOutline />
               </div>
-              <div className="text-sm text-gray-600">
-                조회수 {viewCount}
-              </div>
-              <div  className="flex items-center gap-1">
-                <div className="text-sm text-gray-600">
-                  <IoChatbubbleEllipsesOutline />
-                </div>
-                <div className="text-sm text-gray-600">
-                  {answers}
-                </div>
-              </div>
+              <div className="text-sm text-gray-600">{answers}</div>
             </div>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }

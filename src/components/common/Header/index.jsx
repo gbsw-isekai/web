fe5897cf8655b4f783d24b4cc0677aaea6d787c7
jeongwoo <105404297+lliilili@users.noreply.github.com>
2024-01-03 21:16:15 +0900

    2024.01.03
    디자ㅏㅏㅏㅏㅏ인

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -19,9 +19,9 @@ export default function Header() {
     <div className="border-b py-2">
       <div className="px-4 flex items-center max-w-6xl mx-auto">
         <div className="flex items-center">
-          <GiHorizonRoad className="w-10 h-10" />
           <div className="ml-2">
             <Link to="/">
+              <GiHorizonRoad className="w-10 h-10" />
               지름길
             </Link>
           </div>
@@ -52,7 +52,7 @@ export default function Header() {
           </NavigationMenuList>
         </NavigationMenu>
         <div className="flex-1"></div>
-        <Link to={token ? "/logout" : "/login"} className="self-end">
+        <Link to={token ? "/auth/logout" : "/auth/login"} className="self-end">
           <Button className="w-24">{token ? "로그아웃" : "로그인"}</Button>
         </Link>
       </div>

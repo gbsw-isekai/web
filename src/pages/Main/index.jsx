f43da1296232c8edfe1b4d3c6e09cb669dbcd36d
jeongwoo <105404297+lliilili@users.noreply.github.com>
2024-01-03 12:04:04 +0900

    2024.01.03-(2)
    2차 더미데이터 수정

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -18,21 +18,21 @@ export default function Main() {
 
   const toggleContents = {
     beforeAdmission: [
-      '연애할 수 있을까요?',
-      '경소고 선배 예쁜가요?',
-      '경소고 사감 허리 괜찮은가요?',
+      '어떤 학과가 있나요?',
+      '예습하고 들어가면 좋나요?',
+      '입학 전에 공부해야할게 있나요?',
     ],
-    afterAdmission: ['집에 어떻게 가요?', '방에서 라면먹으면 안되나요?', '뭘 공부해야 하나요?'],
-    study: ['뭐를 공부해야해요?', '보안할까요 코딩할까요?', '일반과목 어떻게 해요?'],
-    jobSearch: ['회사 어디가 좋아요?', '돈 많이주는곳 가고싶어요'],
-    afterEmployment: ['아 넣을 데이터가 없다'],
+    afterAdmission: ['무엇을 공부하는게 가장 좋을까요?', '내신관리 어떻게 해야하나요?', '대회를 많이 참여하는게 좋을까요?'],
+    study: ['자격증 따려고 하는데 어떻게 공부를 해야하나요?', '가장 많이 공부하는 언어가 어떤게 있나요?', '전공만 하고싶은데 일반과목을 굳이 해야하나요?'],
+    jobSearch: ['어떤 회사가 좋은가요?', '인턴쉽을 어디로 가야할까요?', '포트폴리오 어떻게 작성해요?'],
+    afterEmployment: ['회사 생활할 때 팁이 있을까요?'],
   };
 
   return (
-    <div>
+    <div> 
       <Header />
       <div className='text-center mt-10 text-xl font-bold'>
-        자주 묻는 질문
+        Q. 자주 묻는 질문
       </div>
       <div className="flex justify-center mt-8">
         <div className="grid grid-cols-5 gap-6">
@@ -54,7 +54,9 @@ export default function Main() {
       <div className="mt-8 text-center">
         <div className="max-w-md mx-auto">
           {toggleContents[activeToggle].map((content, index) => (
-            <p key={index} className="mb-2 text-gray-800">{content}</p>
+          <div key={index} className="bg-white p-4 mb-4 rounded-md shadow-md">
+            <p className="text-gray-600">{content}</p>
+          </div>
           ))}
         </div>
       </div>

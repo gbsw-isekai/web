2f28215a6e7322ea9768b7083348d5ccaa951b60
wjdgotjd <wjdgotjd529@naver.com>
2024-01-05 03:00:13 +0900

    코드 정리

--- src/pages/Company/detail.jsx
+++ src/pages/Company/detail.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import useToken from "../../hooks/useToken";
 import { useNavigate, useParams } from "react-router-dom";
-import { createCompanyComment, createCompanyReview } from "src/lib/company";
+import {
+  companyViewCount,
+  getCompany,
+  createCompanyComment,
+  createCompanyReview,
+} from "src/lib/company";
 import Header from "src/components/common/Header";
 import { Briefcase, MapPin } from "lucide-react";
 import CompanyReview from "src/components/company/review";
@@ -126,19 +131,10 @@ const Detail = () => {
   };
 
   useEffect(() => {
-    const companyView = async () => {
-      try {
-        await companyViewCount(companiesId, token);
-      } catch (error) {
-        console.error("조회수 업데이트 중 오류가 발생했습니다.", error);
-      }
-    };
-
     if (!token || !userId) {
       alert("로그인을 하셔야 회사 정보를 조회할 수 있습니다.");
       navigate("/auth/login");
     }
-    companyView();
   }, [token, userId, navigate]);
 
   if (isCompanyLoading) {
@@ -223,7 +219,7 @@ const Detail = () => {
         </div>
         <div className="border rounded-md p-4 flex flex-col gap-2">
           <div className="text-lg font-bold">소셜</div>
-          <CompanyCommentList comments={comments ?? []} token={token} />
+          <CompanyCommentList comments={comments ?? []} />
           <CompanyCommentForm
             placeholder="댓글을 입력하세요."
             onSubmit={handleCommentSubmit}
@@ -270,6 +266,7 @@ const Detail = () => {
 export default Detail;
 
 const EmployeeGraph = ({ companyNpsEmployeeData }) => {
+  console.log(companyNpsEmployeeData);
   if (!companyNpsEmployeeData) {
     return <></>;
   }
@@ -281,6 +278,8 @@ const EmployeeGraph = ({ companyNpsEmployeeData }) => {
   const monthlyPrices = companyNpsEmployeeData.map(
     (data) => data.monthlyPrice / 0.09 / data.total
   );
+
+  console.log(companyNpsEmployeeData);
 
   const data = {
     labels: months,
